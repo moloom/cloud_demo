ca1cf3c34c962816e63fc786bdd6061539d1925d
moloom <moloom777@gmail.com>
2023-09-07 13:00:35 +0800

    addition user info in searched order info

--- order-service/src/main/java/com/mo/moloom/service/OrderService.java
+++ order-service/src/main/java/com/mo/moloom/service/OrderService.java
@@ -2,7 +2,9 @@ package com.mo.moloom.service;
 
 import com.mo.moloom.mapper.OrderMapper;
 import com.mo.moloom.pojo.Order;
+import com.mo.moloom.pojo.User;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
@@ -12,8 +14,17 @@ public class OrderService {
     @Resource
     private OrderMapper orderMapper;
 
+    @Resource
+    private RestTemplate restTemplate;
+
+    private static final String URL = "http://localhost:8081/user/";
+
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
-        return orderMapper.findById(orderId);
+        Order order = orderMapper.findById(orderId);
+        // 获取user信息
+        User user = restTemplate.getForObject(URL, User.class, order.getUserId());
+        order.setUser(user);
+        return order;
     }
 }
