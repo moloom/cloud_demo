03989c64b0a049fbfe9a50c3885a0b4db637354d
moloom <moloom777@gmail.com>
2023-09-08 13:11:51 +0800

    addition eureka into project

---
+++ eureka-server/src/main/java/com/mo/moloom/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.mo.moloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: moloom
+ * @date: 2023-09-08 10:56
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
--- order-service/src/main/java/com/mo/moloom/OrderApplication.java
+++ order-service/src/main/java/com/mo/moloom/OrderApplication.java
@@ -3,10 +3,12 @@ package com.mo.moloom;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @MapperScan("com.mo.moloom.mapper")
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
