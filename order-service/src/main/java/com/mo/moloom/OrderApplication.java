cd341a30382af35a1f1fb9ecb5c456235e63474c
moloom <moloom777@gmail.com>
2023-09-13 14:18:24 +0800

    addition nacos

--- order-service/src/main/java/com/mo/moloom/OrderApplication.java
+++ order-service/src/main/java/com/mo/moloom/OrderApplication.java
@@ -3,8 +3,6 @@ package com.mo.moloom;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @MapperScan("com.mo.moloom.mapper")
 @SpringBootApplication
