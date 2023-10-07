032b8fbe387bc5571191d9b198430f05ba346b1a
moloom <moloom777@gmail.com>
2023-10-07 16:01:16 +0800

    addition gateway

---
+++ gateway/src/main/java/com/mo/moloom/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.mo.moloom.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: moloom
+ * @date: 2023-10-07 15:07
+ * @description:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
