910e8167c65c4cf9efee6ccd999ab47e24e77eea
moloom <moloom777@gmail.com>
2023-10-08 00:04:04 +0800

    addition some comment for @EnableFeignClients

--- order-service/src/main/java/com/mo/moloom/OrderApplication.java
+++ order-service/src/main/java/com/mo/moloom/OrderApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+//basePackages指定扫描Feign所在的包，防止Feign通过jar包引入时报错找不到；
+// 此class在com.mo.moloom下，而Feign在com.mo.moloom.clients下，则可以不加
+@EnableFeignClients(basePackages = "com.mo.moloom.clients")
 @MapperScan("com.mo.moloom.mapper")
 @SpringBootApplication
 public class OrderApplication {
